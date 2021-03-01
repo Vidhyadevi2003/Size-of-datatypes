bb5755ae4a1d53d1d1efca81bb0d3f6107425d05
Vidhyadevi2003 <78740193+Vidhyadevi2003@users.noreply.github.com>
2021-03-01 10:32:05 +0530

    Created Product.c

---
+++ Product.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,pro;
+    cout<<"Enter Two Numbers:";
+    cin>>a>>b;
+    pro=a*b;
+    cout<<"Product="<<pro;
+    return 0;
+}
